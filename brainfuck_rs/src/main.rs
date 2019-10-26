aa27e7887a470415ff81c423dde2f5bd09312fd1
5FiftySix6 <ybham6@gmail.com>
2019-10-26 16:22:27 -0400

    Optimize rust a bit

--- brainfuck_rs/src/main.rs
+++ brainfuck_rs/src/main.rs
@@ -5,10 +5,8 @@ use std::num::Wrapping;
 
 #[derive(Debug)]
 enum Instruction {
-    Inc,
-    Dec,
-    Forward,
-    Back,
+    Inc(i8),
+    Ptr(isize),
     Loop(Vec<Instruction>),
     Read,
     Write,
@@ -20,42 +18,61 @@ fn parse(program: &str) -> Vec<Instruction> {
     let mut loop_ind = 0;
 
     for (ind, i) in program.chars().enumerate() {
+        if loop_stack != 0 {
+            match i {
+                '[' => loop_stack += 1,
+                ']' => {
+                    loop_stack -= 1;
+
+                    if loop_stack == 0 {
+                        instrs.push(Instruction::Loop(parse(&program[loop_ind + 1..ind])))
+                    }
+                }
+                _ => (),
+            }
+
+            continue;
+        }
+
+        if i == '>' || i == '<' {
+            if let Some(Instruction::Ptr(ref mut shift)) = instrs.last_mut() {
+                *shift += match i {
+                    '>' => 1,
+                    '<' => -1,
+                    _ => unreachable!(),
+                };
+
+                continue;
+            }
+        }
+
+        if i == '+' || i == '-' {
+            if let Some(Instruction::Inc(ref mut shift)) = instrs.last_mut() {
+                *shift += match i {
+                    '+' => 1,
+                    '-' => -1,
+                    _ => unreachable!(),
+                };
+
+                continue;
+            }
+        }
+
         if let Some(instr) = {
-            if loop_stack == 0 {
-                match i {
-                    '>' => Some(Instruction::Forward),
-                    '<' => Some(Instruction::Back),
-                    '+' => Some(Instruction::Inc),
-                    '-' => Some(Instruction::Dec),
-                    ',' => Some(Instruction::Read),
-                    '.' => Some(Instruction::Write),
-                    '[' => {
-                        loop_stack += 1;
-                        loop_ind = ind;
-                        None
-                    }
-                    ']' => panic!("] at {} has no matching [", ind),
-                    _ => None,
+            match i {
+                '>' => Some(Instruction::Ptr(1)),
+                '<' => Some(Instruction::Ptr(-1)),
+                '+' => Some(Instruction::Inc(1)),
+                '-' => Some(Instruction::Inc(-1)),
+                ',' => Some(Instruction::Read),
+                '.' => Some(Instruction::Write),
+                '[' => {
+                    loop_stack += 1;
+                    loop_ind = ind;
+                    None
                 }
-            } else {
-                match i {
-                    '[' => {
-                        loop_stack += 1;
-                        None
-                    }
-                    ']' => {
-                        let start = loop_ind;
-
-                        loop_stack -= 1;
-
-                        if loop_stack == 0 {
-                            Some(Instruction::Loop(parse(&program[start + 1..ind])))
-                        } else {
-                            None
-                        }
-                    }
-                    _ => None,
-                }
+                ']' => panic!("] at {} has no matching [", ind),
+                _ => None,
             }
         } {
             instrs.push(instr)
@@ -76,10 +93,8 @@ fn run(instrs: Vec<Instruction>) {
 fn run_mem(instrs: &[Instruction], mem: &mut [Wrapping<u8>; 30000], offset: &mut usize) {
     for i in instrs {
         match i {
-            Instruction::Inc => mem[*offset] += Wrapping(1),
-            Instruction::Dec => mem[*offset] -= Wrapping(1),
-            Instruction::Forward => *offset += 1,
-            Instruction::Back => *offset -= 1,
+            Instruction::Inc(i) => mem[*offset] += Wrapping(*i as u8),
+            Instruction::Ptr(shift) => *offset = offset.wrapping_add(*shift as usize),
             Instruction::Loop(sub) => {
                 while mem[*offset].0 != 0 {
                     run_mem(&sub, mem, offset)
@@ -96,25 +111,41 @@ fn run_mem(instrs: &[Instruction], mem: &mut [Wrapping<u8>; 30000], offset: &mut
             }
             Instruction::Write => {
                 print!("{}", mem[*offset].0 as char);
-                stdout().flush().ok().expect("Could not flush stdout.");
+                stdout().flush().expect("Could not flush stdout.");
             }
         }
     }
 }
 
-fn main() -> std::io::Result<()> {
+fn read_file(f_name: &str, contents: &mut String) -> Result<(), std::io::Error> {
+    let mut file = File::open(f_name)?;
+    file.read_to_string(contents)?;
+
+    Ok(())
+}
+
+fn main() -> Result<(), String> {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() > 2 {
-        panic!("Got more than 1 argument! Usage: {} FILE", &args[0]);
+    if args.len() > 3 {
+        // writeln!("Got more than 2 arguments! Usage: {} FILE", &args[0]);
+        return Err(format!(
+            "Got more than 2 arguments! Usage: {} FILE",
+            &args[0]
+        ));
     } else if args.len() == 1 {
-        panic!("Got no file argument! Usage: {} FILE", &args[0]);
+        return Err(format!(
+            "Got no file argument! Usage: {} FILE [OUTPUT]",
+            &args[0]
+        ));
     }
 
+    // let out = if args.len() == 2 { "a.out" } else { &args[2] };
+
     let mut contents = String::new();
-    {
-        let mut file = File::open(&args[1])?;
-        file.read_to_string(&mut contents)?;
+
+    if let Err(err) = read_file(&args[1], &mut contents) {
+        return Err(format!("Unable to read file! {}", err));
     }
 
     run(parse(&contents));
